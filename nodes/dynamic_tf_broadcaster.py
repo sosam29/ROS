044f0103fb75882e0c6ea216ea3ba4217d91ef57
sosam29 <sosam29@gmail.com>
2019-06-10 21:27:05 -0400

    testing more funcationality

---
+++ nodes/dynamic_tf_broadcaster.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import rospy
+import roslib
+roslib.load_manifest('learning_tf')
+
+import tf
+import math
+
+
+if __name__ == "__main__":
+    rospy.init_node('dynamic_tf_broadcaster')
+    br = tf.TransformBroadcaster()
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown():
+        t = rospy.Time.now().to_sec()*math.pi
+        br.sendTransform((2 *math.sin(t), 2 * math.cos(t), 0.0),(0.0,0.0,0.0,1.0),rospy.Time.now(), "carrot1", "turtle1")
+        rate.sleep()
--- nodes/turtle_tf_listner.py
+++ nodes/turtle_tf_listner.py
@@ -18,12 +18,16 @@ if __name__ == "__main__":
 
     turtle_vel = rospy.Publisher('turtle2/cmd_vel', geometry_msgs.msg.Twist, queue_size=1)
     rate = rospy.Rate(10.0)
+    listner.waitForTransform('/turtle2','/carrot1', rospy.Time(), rospy.Duration(4.0))
 
     while not rospy.is_shutdown():
         try:
-            (trans, rot)=listner.lookupTransform('/turtle2','/turtle1', rospy.Time(0))
-
-        except (tf.LookupException,tf.ConnectivityException, tf.ExtrapolationException):
+            # (trans, rot)=listner.lookupTransform('/turtle2','/carrot1', rospy.Time(0))
+            now = rospy.Time.now()
+            past = now - rospy.Duration(5.0)
+            listner.waitForTransformFull("/turtle2", now, "/turtle1", past, '/world',rospy.Duration(1.0))
+            (trans, rot) = listner.lookupTransformFull('/turtle2',now, '/turtle1', past,  '/world')
+        except (tf.LookupException,tf.ConnectivityException, tf.Exception):
             continue
         angular = 4 * math.atan2(trans[1], trans[0])
         linear = 0.5 * math.sqrt(trans[0]**2 + trans[1]**2)
